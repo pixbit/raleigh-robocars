43a5146f87ba379c9fc7f1221e788be9d147bb78
Seth Rhodes <seth@thinkpixbit.com>
2016-10-18 16:01:40 -0400

    index: import the HomePageComponent

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 // let jQuery = require('./js/vendor/jquery');
 // let foundation = import './js/vendor/foundation';
 // import './js/app';
-//
+
 import React from 'react';
 import {render} from 'react-dom';
-let TitleComponent = () => {
-  return (<h1>Title</h1>);
-};
-let NavbarComponent = () => {
-  return (
-    <header>
-      <nav className="row align-justify">
-        <TitleComponent />
-        <ul className="menu small-4 column">
-          <li><a href="#how-it-works">How it works</a></li>
-          <li><a href="#on-the-road">On the Road</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
-render(<NavbarComponent />, document.getElementById('app'));
+import HomePageComponent from './components/HomePageComponent';
+render(<HomePageComponent />, document.getElementById('app'));
