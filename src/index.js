f76e04b5261ff999f5d0ffae993290963c5ff6e1
Seth Rhodes <seth@thinkpixbit.com>
2016-10-18 15:34:35 -0400

    index: Navbar & Title component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import './js/vendor/jquery';
-import './js/vendor/foundation';
-import './js/app';
+// let jQuery = require('./js/vendor/jquery');
+// let foundation = import './js/vendor/foundation';
+// import './js/app';
+//
+import React from 'react';
+import {render} from 'react-dom';
+let TitleComponent = () => {
+  return (<h1>Title</h1>);
+};
+let NavbarComponent = () => {
+  return (
+    <header>
+      <nav className="row align-justify">
+        <TitleComponent />
+        <ul className="menu small-4 column">
+          <li><a href="#how-it-works">How it works</a></li>
+          <li><a href="#on-the-road">On the Road</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+render(<NavbarComponent />, document.getElementById('app'));
