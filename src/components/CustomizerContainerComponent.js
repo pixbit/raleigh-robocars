ba8e7626d57514d7291b3bbb7b027587d92cbe85
Seth Rhodes <seth@thinkpixbit.com>
2016-10-26 23:28:20 -0400

    CustomizerContainerComponent: styling added to component

--- src/components/CustomizerContainerComponent.js
+++ src/components/CustomizerContainerComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddOnsComponent from './AddOnsComponent';
 import ColorPickerComponent from './ColorPickerComponent';
+import './CustomizerContainerComponent.scss';
 class CustomizerContainerComponent extends React.Component {
   constructor() {
     super();
