dbb570bac9afa53bdbc9f672977e3bfc9e788366
Seth Rhodes <seth@thinkpixbit.com>
2016-10-27 18:02:13 -0400

    CustomizerContainer: top-level state and passing of props

--- src/components/CustomizerContainerComponent.js
+++ src/components/CustomizerContainerComponent.js
@@ -1,15 +1,31 @@
 import React from 'react';
+
 import AddOnsComponent from './AddOnsComponent';
 import ColorPickerComponent from './ColorPickerComponent';
+import CarPreviewComponent from './CarPreviewComponent';
+import CustomizerHeaderComponent from './CustomizerHeaderComponent';
 import './CustomizerContainerComponent.scss';
+
 class CustomizerContainerComponent extends React.Component {
   constructor() {
     super();
+    this.state = {
+      carColor: 'blue'
+    };
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  changeColor(newCarColor) {
+    this.setState({
+      carColor: newCarColor
+    });
   }
   render() {
     return (
-      <div id="customizer">
-        <ColorPickerComponent />
+      <div id="customizer" className="row">
+        <CustomizerHeaderComponent />
+        <ColorPickerComponent changeColor={this.changeColor} carColor={this.state.carColor}/>
+        <CarPreviewComponent carColor={this.state.carColor} />
         <AddOnsComponent />
       </div>
     );
