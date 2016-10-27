4b811ebb328e3ee8a893fd6e955b9005f69673b6
Seth Rhodes <seth@thinkpixbit.com>
2016-10-27 18:02:40 -0400

    CustomizerHeaderComponent :sunglasses:

---
+++ src/components/CustomizerHeaderComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CustomizerHeaderComponent.scss';
+const CustomizerHeaderComponent = () => {
+  return (
+    <div className="columns small-12" id="customizer-header">
+      Works
+    </div>
+  );
+};
+
+export default CustomizerHeaderComponent;
